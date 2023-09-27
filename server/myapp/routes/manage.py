dae364692e27b6a1becfb2e6758484bbbe760ffc
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-09-27 05:31:51 -0400

    edited manage.py

--- server/myapp/routes/manage.py
+++ server/myapp/routes/manage.py
@@ -1,7 +1,8 @@
 from main import app, db
-from flask_migrate import MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command("db", MigrateCommand)
 
